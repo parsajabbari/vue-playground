fca583ad61ab9a3c13429a4732125d575d8297c8
Parsa Jabbari <parsajabbari@gmail.com>
2022-03-04 19:32:09 -0800

    feat: put more realistic welcome message

--- vue-playground/src/App.vue
+++ vue-playground/src/App.vue
@@ -14,7 +14,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
     />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Holy shit it actually worked!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
